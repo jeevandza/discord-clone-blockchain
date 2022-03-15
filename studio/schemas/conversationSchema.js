a3a97a52984d44464cbf4167852e83e8ace01aa7
Jeevan <jeevandza4@gmail.com>
2022-03-15 14:53:21 +0530

    sanity setup

---
+++ studio/schemas/conversationSchema.js
@@ -0,0 +1,36 @@
+
+
+
+
+export const conversationSchema = {
+  name: "conversations",
+  type: "document",
+  title: "Converstaions",
+  fields: [
+    {
+      name: "roomName",
+      type: "string",
+      title: "Room Name",
+    },
+    {
+      name: "roomId",
+      type: "string",
+      title: "Room Id",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+    },
+    {
+      name: "isDm",
+      type: "boolean",
+      title: "Is DM?",
+    },
+    {
+      name: "userReference",
+      type: "reference",
+      to: [{ type: "users" }],
+    },
+  ],
+};
